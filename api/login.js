30d1c8bb8438726951c64a0089c9b2ad2d214d9d
nzofhrzi1291 <157079667+nzofhrzi@users.noreply.github.com>
2026-06-08 15:42:02 +0700

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,12 +1,34 @@
-const res = await fetch('/api/login', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({
-    nim,
-    password
-  })
-})
+const { getFile } = require("./github");
 
-const data = await res.json()
+module.exports = async (req, res) => {
+  try {
+    const { nim, password } = req.body;
+
+    const users = await getFile("data/users.json");
+
+    const user = users.find(
+      u =>
+        u.nim === nim &&
+        u.password === password
+    );
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Login gagal"
+      });
+    }
+
+    delete user.password;
+
+    return res.json({
+      success: true,
+      user
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      error: err.message
+    });
+  }
+};
